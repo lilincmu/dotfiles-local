99ccac86a2a91f74b8ba0258d3170cf86f463186
lilincmu <lilincmu@gmail.com>
2021-02-06 22:26:58 -0800

    Adjust window size vertically/horizontally

--- hammerspoon/README.md
+++ hammerspoon/README.md
@@ -1,10 +1,14 @@
 # Shortcuts
 ## Window
 - hyper + f: fullscreen
-- hyper + =: increase window size
-- hyper + -: increase window size
-- hyper + -> : move window right
-- hyper + <- : move window left
+
+- hyper + ]: increase window size vertically
+- hyper + [: decrease window size vertically
+- hyper + =: increase window size horizontally
+- hyper + -: decrease window size horizontally
+- hyper + hjkl: move window
+- hyper + -> : move window to right screen
+- hyper + <- : move window to left screen
 
 ## App
 - hyper + c: focus Google Chrome
--- hammerspoon/window.lua
+++ hammerspoon/window.lua
@@ -1,4 +1,6 @@
-local STEP = 10
+local STEP = 20
+
+hs.window.animationDuration = 0
 
 -- full screen
 hs.hotkey.bind(HYPER, 'f', function()
@@ -7,22 +9,30 @@ end)
 
 -- resize window
 local resizes = {{
+    key = "]",
+    size = {STEP, 0}
+}, {
+    key = "[",
+    size = {-STEP, 0}
+}, {
     key = "=",
-    size = STEP
+    size = {0, STEP}
 }, {
     key = "-",
-    size = -STEP
+    size = {0, -STEP}
 }}
 
 for i, resize in ipairs(resizes) do
     hs.hotkey.bind(HYPER, resize.key, function()
         local win = hs.window.focusedWindow()
         local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-        f.x = f.x - resize.size
-        f.y = f.y - resize.size
-        f.w = f.w + resize.size * 2
-        f.h = f.h + resize.size * 2
+        f.x = math.max(f.x - resize.size[1], max.x)
+        f.y = math.max(f.y - resize.size[2], max.y)
+        f.w = f.w + resize.size[1] * 2
+        f.h = f.h + resize.size[2] * 2
         win:setFrame(f)
     end)
 end
@@ -34,7 +44,7 @@ local moves = {{
 }, {
     key = "l",
     direction = {STEP, 0}
-},{
+}, {
     key = "j",
     direction = {0, STEP}
 }, {
@@ -46,9 +56,11 @@ for i, move in ipairs(moves) do
     hs.hotkey.bind(HYPER, move.key, function()
         local win = hs.window.focusedWindow()
         local f = win:frame()
-    
-        f.x = f.x + move.direction[1]
-        f.y = f.y + move.direction[2]
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = math.max(math.min(f.x + move.direction[1], max.x + (max.w - f.w)), max.x)
+        f.y = math.max(math.min(f.y + move.direction[2], max.y + (max.h - f.h)), max.y)
         win:setFrame(f)
     end)
 end
