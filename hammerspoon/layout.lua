f5020f8946be308a6e022de2b3db60c8bfa0ac32
lilincmu <lilincmu@gmail.com>
2021-06-27 12:14:36 -0700

    Adjust home layout

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -13,11 +13,11 @@ LAYOUT_OFFICE = {
 }
 
 LAYOUT_HOME = {
-    {'Calendar', MACBOOK_MONITOR, 1, hs.geometry.unitrect(0, 0, 1, 1)},
-    {'Google Chrome', MIDDLE_MONITOR, 1, hs.geometry.unitrect(0, 0, 1, 1)},
+    {'Calendar', MIDDLE_MONITOR, 1, hs.geometry.unitrect(0, 0, 1, 1)},
+    {'Google Chrome', MIDDLE_MONITOR, 2, hs.geometry.unitrect(0, 0, 1, 1)},
     {'Terminal', MIDDLE_MONITOR, 2, hs.geometry.rect(600, 150, 650, 750)},
-    {'Code', MIDDLE_MONITOR, 2, hs.geometry.unitrect(0, 0, 1, 1)},
-    {'Microsoft OneNote', MIDDLE_MONITOR, 3, hs.geometry.unitrect(0, 0, 1, 1)},
+    {'Code', MIDDLE_MONITOR, 3, hs.geometry.unitrect(0, 0, 1, 1)},
+    {'Microsoft OneNote', MIDDLE_MONITOR, 4, hs.geometry.unitrect(0, 0, 1, 1)},
 }
 
 LAYOUT_LAPTOP = {
